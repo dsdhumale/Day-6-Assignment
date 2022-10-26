0181bee492df9c22fa2930c0ea7111210b4dd33f
dsdhumale <112741544+dsdhumale@users.noreply.github.com>
2022-10-26 12:28:37 +0530

    Added : Ability to convert temperature in farad to celicius

---
+++ TemperatureConversion.java
@@ -0,0 +1,18 @@
+package day6Problem;
+
+import java.util.Scanner;
+
+public class TemperatureConversion {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter  Fahrenheit temperature :");
+		double temp = scanner.nextDouble();
+
+		System.out.println("Celsius temperature is = " + celsius(temp));
+	}
+
+	static double celsius(double f) {
+		return (f - 32) * 5 / 9;
+	}
+}
